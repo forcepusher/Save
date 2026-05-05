d32acf27fb599f634f6072d96aef0c59761dae79
forcepusher <bionitsoup@gmail.com>
2026-05-05 22:18:07 +0700

    Remove useless stuff

--- Packages/com.bananaparty.save/Runtime/IStateStorage.cs
+++ Packages/com.bananaparty.save/Runtime/IStateStorage.cs
@@ -19,12 +19,6 @@ namespace BananaParty.Save
         void SaveFloat(string key, float value);
         float LoadFloat(string key, float defaultValue = 0f);
 
-        void SaveDouble(string key, double value);
-        double LoadDouble(string key, double defaultValue = 0.0);
-
-        void SaveLong(string key, long value);
-        long LoadLong(string key, long defaultValue = 0L);
-
         void SaveBool(string key, bool value);
         bool LoadBool(string key, bool defaultValue = false);
 
@@ -37,9 +31,6 @@ namespace BananaParty.Save
 
         void SaveVector3(string key, Vector3 value);
         Vector3 LoadVector3(string key, Vector3 defaultValue = default);
-
-        void SaveVector4(string key, Vector4 value);
-        Vector4 LoadVector4(string key, Vector4 defaultValue = default);
 
         // Unity integer vectors
         void SaveVector2Int(string key, Vector2Int value);
@@ -55,15 +46,9 @@ namespace BananaParty.Save
         void SaveColor(string key, Color value);
         Color LoadColor(string key, Color defaultValue = default);
 
-        void SaveColor32(string key, Color32 value);
-        Color32 LoadColor32(string key, Color32 defaultValue = default);
-
         // Date/time types
         void SaveDateTime(string key, DateTime value);
         DateTime LoadDateTime(string key, DateTime defaultValue = default);
-
-        void SaveTimeSpan(string key, TimeSpan value);
-        TimeSpan LoadTimeSpan(string key, TimeSpan defaultValue = default);
 
         // Collections - removed class constraint to support value types
         void SaveList<T>(string key, List<T> value);
@@ -73,21 +58,9 @@ namespace BananaParty.Save
         void SaveDictionary<TKey, TValue>(string key, Dictionary<TKey, TValue> value) where TKey : notnull;
         Dictionary<TKey, TValue> LoadDictionary<TKey, TValue>(string key) where TKey : notnull;
 
-        // Array support
-        void SaveStringArray(string key, string[] value);
-        string[] LoadStringArray(string key);
-
-        void SaveIntArray(string key, int[] value);
-        int[] LoadIntArray(string key);
-
         void SaveByteArray(string key, byte[] value);
         byte[] LoadByteArray(string key);
 
-        // GUID support
-        void SaveGuid(string key, Guid value);
-        Guid LoadGuid(string key, Guid defaultValue = default);
-
-        // Enum support (stored as string for versioning safety)
         void SaveEnum<T>(string key, T value) where T : Enum;
         T LoadEnum<T>(string key, T defaultValue = default) where T : Enum;
 
